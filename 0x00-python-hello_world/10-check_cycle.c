f2aea3e722d968a3d845b13aceca227836bec686
Jotham333 <jothamjosiah10@gmail.com>
2023-06-06 03:35:37 +0000

    ALX

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a code has cycle 
+ *
+ * @head: the head of the linked list
+ *
+ * Return: 1 if cycle exit and 1 otherwise
+ */
+
+
+
+int check_cycle(listint_t *head)
+{
+	listint_t *slow = head;
+	listint_t *fast = head->next;
+
+	if (slow == NULL || fast == NULL)
+	{
+		return (0);
+	}
+	while (slow != NULL && fast != NULL && fast->next != NULL)
+	{
+	
+		if (slow == fast)
+		{
+			return (1);	
+		}
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+
+	return (0);
+}

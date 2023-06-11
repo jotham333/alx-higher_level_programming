106e49d7e79db30c1cd086d5d206d102af0fa4d3
Jotham333 <jothamjosiah10@gmail.com>
2023-06-11 12:14:41 +0000

    ALX

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+def element_at(my_list, idx):
+
+    """
+    element_at: retrieve the element at a specific index in the list
+
+    Args:
+        my_list: the list
+        idx: the index
+
+    Returns:
+        The element at the given index, or None if the index is out of range
+    """
+    list_len = len(my_list)
+
+    if idx >= list_len or idx < 0:
+            return (None)
+
+    return (my_list[idx])

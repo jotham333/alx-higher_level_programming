bb24f538a6e2e15eaaf292fb42215f0d769df7e3
Jotham333 <jothamjosiah10@gmail.com>
2023-06-11 11:34:57 +0000

    ALX

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def print_list_integer(my_list=[]):
+
+    """ print_list_intger : print the content of a list
+
+        Args:
+                my_list: the list """
+
+
+    for i in my_list:
+        print("{}".format(i))

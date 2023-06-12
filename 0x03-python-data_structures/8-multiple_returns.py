2cccdb2c7e8ada99a916bb016be2ac5ea9f590dd
Jotham333 <jothamjosiah10@gmail.com>
2023-06-12 12:27:04 +0000

    ALX

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,20 +1,9 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-
-    """
-        multiple: returns multiple variable
-
-        Args:
-            sentence: The parameter
-
-        Returns: multiple variable
-    """
     my_tuple = ()
-
     if len(sentence) == 0:
         my_tuple = 0, "None"
-
     else:
         my_tuple = len(sentence), sentence[0]
-        return my_tuple
+    return my_tuple

e5b9f6ea75b9e46f4ddecdb05cf72cbc0900bb7c
Jotham333 <jothamjosiah10@gmail.com>
2023-06-12 05:10:27 +0000

    ALX

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+
+    if len(my_list) == 0:
+        return None
+
+    max_digit = my_list[0]
+
+    for i in range(1, len(my_list)):
+        if my_list[i] > max_digit:
+            max_digit = my_list[i]
+
+    return max_digit

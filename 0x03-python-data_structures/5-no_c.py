b6504fd6e40a4c8e153a4151c718ac77bb1238da
Jotham333 <jothamjosiah10@gmail.com>
2023-06-12 01:32:18 +0000

    ALX

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -7,10 +7,10 @@ def no_c(my_string):
 
         Args:
             my_string: the string literal
-        
+
         Returns:the new string
 
     """
 
-    new_string = my_string.translate({ord(i): None for i in 'cC'}) 
+    new_string = my_string.translate({ord(i): None for i in 'cC'})
     return new_string

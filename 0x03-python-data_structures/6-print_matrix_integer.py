56ef3a92c6469503f0ad63facebd75582eb65176
Jotham333 <jothamjosiah10@gmail.com>
2023-06-12 02:04:42 +0000

    ALX

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+
+    """
+
+
+    """
+
+    for row in matrix:
+        for element in row:
+            print(element, end=" ")
+        print()

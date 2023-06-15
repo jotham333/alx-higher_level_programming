207d0af7657a83d7193209536f6a2a09e8935869
Jotham333 <jothamjosiah10@gmail.com>
2023-06-15 01:28:56 +0000

    ALX

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+    squared_matrix = []
+
+    for row in matrix:
+        squared_row = list(map(lambda element: element ** 2, row))
+        squared_matrix.append(squared_row)
+
+    return (squared_matrix)

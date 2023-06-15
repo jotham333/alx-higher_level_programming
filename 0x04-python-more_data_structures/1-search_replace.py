623149b60f9afddb2392c06449de847eda39f0ee
Jotham333 <jothamjosiah10@gmail.com>
2023-06-15 02:17:01 +0000

    ALX

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
 
-
 def search_replace(my_list, search, replace):
-    new_list = []
-    for i in range(len(my_list)):
-        if my_list[i] == search:
-            my_list[i] = replace
-            new_list.append(my_list[i])
-        else:
-            new_list.append(my_list[i])
-
+    new_list = my_list[:]
+    for i in range(len(new_list)):
+        if new_list[i] == search:
+            new_list[i] = replace
     return (new_list)

34009374eb752c82ae05653d261f34d80e1d5913
Jotham333 <jothamjosiah10@gmail.com>
2023-06-27 04:42:35 +0000

    ALX

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-
 def safe_print_list(my_list=[], x=0):
 
     count = 0
@@ -8,9 +7,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{}".format(my_list[i]), end='')
             count +=1
-
         except IndexError:
             break
-
     print()
     return count
--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,17 +1,11 @@
 #!/usr/bin/python3
 
-
 def safe_print_division(a, b):
-    
-    """Returns the division of a by b."""
 
     try:
         result = a / b
-
     except (TypeError, ZeroDivisionError):
         result = None
-
     finally:
-        print("{}".format(result))
-   
+        iprint("Inside result: {}".format(result))
     return (result)

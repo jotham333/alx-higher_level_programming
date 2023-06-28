e3c1e9157b2504f00786085bc33a05ee0f3f0211
Jotham333 <jothamjosiah10@gmail.com>
2023-06-28 02:40:03 +0000

    ALX

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+"""Define a class Square"""
+
+
+class Square:
+    """Represent the Square"""
+
+    def __init__(self, size):
+        
+        """Initialize a new Square
+
+        Args:
+            size (int): The size of the new square.
+        """
+
+        self.__size = size

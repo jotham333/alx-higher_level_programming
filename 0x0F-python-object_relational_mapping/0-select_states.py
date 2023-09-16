ecd3177a098d315cfc6ea03b5a2e16ff279418af
jotham333 <jothamjosiah10.com>
2023-09-16 18:29:09 +0100

    Python ORM

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -11,9 +11,9 @@ username = sys.argv[1]
 password = sys.argv[2]
 database_name = sys.argv[3]
 
-try:
-    db = MySQLdb.connect(host="localhost", user="jotham333",
-                         passwd="jotham087", db="hbtn_0e_0_usa")
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=username,
+                         passwd=password, db=database_name, port=3306)
     cur = db.cursor()
 
     cur.execute("SELECT * FROM states ORDER BY states.id;")
@@ -23,12 +23,5 @@ try:
     for state in states:
         print(state)
 
-except MySQLdb.Error as e:
-    print(f"Error: {e}")
-
-finally:
-    if 'db' in locals() and db is not None:
-        db.close()
-
-    if 'cur' in locals() and cur is not None:
-        cur.close()
+    db.close()
+    cur.close()

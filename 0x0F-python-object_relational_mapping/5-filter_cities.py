7192d5abc3cd976e8049277d1939d6125ba80f0a
jotham333 <jothamjosiah10.com>
2023-09-17 14:01:42 +0100

    Pyhton ORM

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -4,8 +4,8 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2],
-                         db=sys.argv[3], port=3306)
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
     cur.execute("""
                     SELECT cities.name
@@ -13,8 +13,8 @@ if __name__ == "__main__":
                     INNER JOIN states on states.id=cities.state_id
                     WHERE states.name=%s""", (sys.argv[4],))
     state = cur.fetchall()
-    for cities in state:
-        print(cities, end=" ")
+    tmp = list(city[0] for city in state)
+    print(*tmp, sep=", ")
 
     cur.close()
     db.close()

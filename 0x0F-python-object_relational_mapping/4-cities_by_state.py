a2386091335df9a71e522ce8d5f11633a9b6b911
jotham333 <jothamjosiah10.com>
2023-09-17 13:49:00 +0100

    Python ORM

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     cur.execute("""
         SELECT cities.id, cities.name, states.name
         FROM cities
-        INNER JOIN states ON states.id = cities.state_id
+        INNER JOIN states ON states.id=cities.state_id
         """)
     states = cur.fetchall()
     for state in states:

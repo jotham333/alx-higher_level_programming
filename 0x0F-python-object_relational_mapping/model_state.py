a2461735380408d71198e8b6d5448477cbe4ad8e
jotham333 <jothamjosiah10.com>
2023-09-17 15:58:17 +0100

    python ORM

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""Contains State class and Base, an instance of declarative_base()  """
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    Class with id and name attribute of each state
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False, unique=True)
+    name = Column(String(128), nullable=False)

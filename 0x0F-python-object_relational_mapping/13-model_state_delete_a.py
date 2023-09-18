21dbfef70dc32ebbd12de93e158bce49a3e8e967
jotham333 <jothamjosiah10.com>
2023-09-18 04:52:05 +0100

    Python ORM

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-""" prints the first State object from the database hbtn_0e_6_usa
- """
+""" prints the State object with the name passed as argument from the database
+"""
 import sys
-from model_state import State, Base
+from model_state import Base, State
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
+
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    instance = session.query(State).first()
-    if instance is None:
-        print("Nothing")
-    else:
-        print(instance.id, instance.name, sep=": ")
+    for instance in session.query(State).filter(State.name.like('%a%')):
+        session.delete(instance)
+    session.commit()
